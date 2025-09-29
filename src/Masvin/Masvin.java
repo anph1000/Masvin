90fc53d247f35bfa17c9eb0ab9496964d3fe314e
anph1000 <anph1000@stud.ek.dk>
2025-09-29 15:02:42 +0200

    v2

--- src/Masvin/Masvin.java
+++ src/Masvin/Masvin.java
@@ -1,4 +1,20 @@
 package Masvin;
 
 public class Masvin {
+        private String name;
+        private String breed;
+        private int food;
+
+        public Masvin (String name, String breed, int food) {
+            this.name = name;
+            this.breed = breed;
+            this.food = food;
+        }
+
+        @Override
+        public String toString() {
+            return name + " (" + breed + "," + food + ")";
+        }
+    }
+
 }
